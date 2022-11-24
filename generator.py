69fc5905134c696cd358bfc74de5d030bbbf14f0
MDev123 <86262715+MDev123@users.noreply.github.com>
2022-11-23 21:25:31 -0500

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,13 @@
+import requests
+from random import randint
+
+url = 'https://svnweb.freebsd.org/csrg/share...
+
+r = requests.get(url)
+text = r.text
+# print(text)
+individual_words = text.split()
+# print(individual_words)
+random_number = randint(0,len(individual_words))
+
+print(individual_words[random_number] + str(random_number))
